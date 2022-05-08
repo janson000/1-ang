cce7147561b4b125165521a54ca747449169b068
janson000 <mail4jans@gmail.com>
2022-05-08 21:10:13 +0530

    1 file modified, 1 added 08 May 2022 change 2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component, VERSION } from '@angular/core';
 
+import { interval } from 'rxjs'; //1
+
+import { fromEvent } from 'rxjs'; //2
+
+//1
+
+// Create an Observable that will publish a value on an interval
+const secondsCounter = interval(1000);
+// Subscribe to begin publishing values
+const subscription = secondsCounter.subscribe((n) =>
+  console.log(`It's been ${n + 1} seconds since subscribing!`)
+);
+
+//2
+
+const el = document.getElementById('my-element')!;
+
+// Create an Observable that will publish mouse movements
+const mouseMoves = fromEvent<MouseEvent>(el, 'mousemove');
+
+// Subscribe to start listening for mouse-move events
+const subscription = mouseMoves.subscribe((evt) => {
+  // Log coords of mouse movements
+  console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
+
+  // When the mouse is over the upper-left of the screen,
+  // unsubscribe to stop listening for mouse movements
+  if (evt.clientX < 40 && evt.clientY < 40) {
+    subscription.unsubscribe();
+  }
+});
+//2 end
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
